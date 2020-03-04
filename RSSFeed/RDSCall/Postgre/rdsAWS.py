7747a83bfc22357e14e74d3ef3d41eb186dead87
Joseph <odh.joseph@gmail.com>
2020-03-03 22:31:12 -0600

    Directory add

---
+++ RSSFeed/RDSCall/Postgre/rdsAWS.py
@@ -0,0 +1,11 @@
+import psycopg2 as ps
+import json
+import sys
+
+# conn = ps.connect(host=credentials['POSTGRES_ADDRESS'],
+#               database=credentials['POSTGRES_DBNAME'],
+#               user=credentials['POSTGRES_USERNAME'],
+#               password=credentials['POSTGRES_PASSWORD'],
+#               port=credentials['POSTGRES_PORT'])
+
+# cur = conn.cursor()
